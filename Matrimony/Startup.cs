60bb1349593a1b68b25548e57b23b81e536bd620
durgesh37 <durgesh3k7@gmail.com>
2017-02-12 21:30:09 +0530

    Add Matrimony Project and Files

---
+++ Matrimony/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Matrimony.Startup))]
+namespace Matrimony
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
